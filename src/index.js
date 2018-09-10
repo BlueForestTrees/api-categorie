7d3f9c3f8b0a886b728bcf7eae5d3251b448d506
slim <smedini@gmail.com>
2018-09-10 18:25:16 +0200

    mineur

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import ENV from "./env"
 import {dbInit} from "mongo-registry"
 import {registry} from "./dbRegistry"
 import startExpress from "express-blueforest"
-import morgan from "morgan"
 
 const errorMapper = err => {
     if (err.code === 11000) {
@@ -10,7 +9,6 @@ const errorMapper = err => {
         err.body = {errorCode: 1, message: "allready exists"}
     }
 }
-const init = api => api.use(morgan(ENV.MORGAN || ':status :method :url :response-time ms - :res[content-length]'))
 
 export default dbInit(ENV, registry)
     .then(startExpress(ENV, errorMapper, init))
